6974b4fa5cb5da3b5a36d27a533e98039c67c431
Mlodziak00 <enderlander@gmail.com>
2024-05-21 20:36:21 +0200

    Changed How ClothingItem Works

--- src/main/java/com/example/init/ItemInit.java
+++ src/main/java/com/example/init/ItemInit.java
@@ -11,8 +11,9 @@ import net.minecraft.util.Identifier;
 public class ItemInit {
     // namespace looks for your mods asset folder
     // texture takes both the "slim_example_clothing" and "example_clothing"
-    public static Item EXAMPLE_ITEM = registerItem("example_clothing", new ClothingItem(new FabricItemSettings().maxCount(1),
-            ExampleClothingMod.MOD_ID, "example_clothing"));
+    // hideHatLayer hides the players Hat Layer
+    public static Item EXAMPLE_ITEM = registerItem("example_clothing", new ClothingItem(
+            ExampleClothingMod.MOD_ID, "example_clothing", false, new FabricItemSettings().maxCount(1)));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, new Identifier(ExampleClothingMod.MOD_ID, name), item);
